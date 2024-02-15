147d358bffed4abeb44793b01330eaeaac557c86
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-02-15 15:56:50 +0200

    Added more templeates for exercises

--- petstagram/petstagram/pets/views.py
+++ petstagram/petstagram/pets/views.py
@@ -11,11 +11,11 @@ def details_pet(request, username, pet_slug):
     return render(request, "pets/details_pet.html", context)
 
 
-def edit_pet(request,username, pet_slug):
+def edit_pet(request, username, pet_slug):
     context = {}
     return render(request, "pets/edit_pet.html", context)
 
 
-def delete_pet(request,username, pet_slug):
+def delete_pet(request, username, pet_slug):
     context = {}
     return render(request, "pets/delete_pet.html", context)
