f65c2848c8f0a1a784479c796b227bca91a468d4
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-02-19 12:33:53 +0200

    Added models

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Pet(models.Model):
@@ -10,11 +11,21 @@ class Pet(models.Model):
 
     date_of_birth = models.DateField(
         blank=True,
-        null=True
+        null=True,
     )
 
     slug = models.SlugField(
         unique=True,
         null=False,
-        blank=True
+        blank=True,
+        editable=False,
     )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        return super().save(*args, **kwargs)
+    
--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-++sxsd_i=@1bwv1&hwuaod+bk-non+msv2e&p$n&g1)2io+=+*'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
