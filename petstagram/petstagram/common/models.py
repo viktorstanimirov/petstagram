9f3f771c5261d6ef74c91ec637a17cf313cf2ffe
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-03-13 15:57:32 +0200

    Added more solutions

--- petstagram/petstagram/common/models.py
+++ petstagram/petstagram/common/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 from petstagram.photos.models import Photo
+
+UserModel = get_user_model()
 
 
 class Comment(models.Model):
@@ -23,9 +26,19 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
     )
 
+    def __str__(self):
+        return self.text
+
 
 class PhotoLike(models.Model):
     pet_photo = models.ForeignKey(
         Photo,
         on_delete=models.DO_NOTHING,
+    )
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.DO_NOTHING,
+        null=True,
+        blank=True,
     )
